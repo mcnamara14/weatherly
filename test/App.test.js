1600d33625d57ce5cbea8696b7f190fdcbea20a8
Tyler McNamara <mcnamara14@gmail.com>
2018-04-16 23:23:46 -0600

    Add hourly forecast testing

--- test/App.test.js
+++ test/App.test.js
@@ -12,43 +12,43 @@ describe('App tests', () => {
 
   beforeEach(() => {
     renderedApp = shallow(<App />);
-  })
+  });
 
   describe('App defaults', () => { 
 
     it('should have default welcome state of true', () => {
       const expectation = '';
-      expect(renderedApp.state('city')).toEqual(expectation)
-    })
+      expect(renderedApp.state('city')).toEqual(expectation);
+    });
 
     it('should have default welcome state of true', () => {
       const expectation = '';
       
-      expect(renderedApp.state('stateUSA')).toEqual(expectation)
-    })
+      expect(renderedApp.state('stateUSA')).toEqual(expectation);
+    });
 
-    // it('should have default weather images', () => {
-    //   const expectation = 'wu wu-black wu-256 wu-clear';
+    it('should have default weather images', () => {
+      const expectation = 'wu wu-black wu-256 wu-clear';
       
-    //   expect(renderedApp.state('images').Clear).toEqual(expectation)
-    // })
+      expect(renderedApp.images.Clear).toEqual(expectation)
+    });
 
     it('should have a default welcome state of true', () => {
       const expectation = true;
 
-      expect(renderedApp.state('welcome')).toEqual(expectation)
-    })
-  })
+      expect(renderedApp.state('welcome')).toEqual(expectation);
+    });
+  });
 
   describe('Render components ', () => { 
     beforeEach(() => {
       renderedApp = shallow(<App />);
-    })
+    });
 
     it('should render the welcome page on new visit', () => {
 
       expect(renderedApp.find('WelcomePage').length).toEqual(1);
-    })
+    });
 
     it('should render all other components when welcome state is false', () => {
       renderedApp.setState({welcome: false});
@@ -57,13 +57,13 @@ describe('App tests', () => {
       expect(renderedApp.find('CurrentWeather').length).toEqual(1);
       expect(renderedApp.find('SevenHour').length).toEqual(1);
       expect(renderedApp.find('TenDay').length).toEqual(1); 
-    })
-  })
+    });
+  });
 
   describe('Update location', () => { 
     beforeEach(() => {
       renderedApp = shallow(<App />);
-    })
+    });
 
     it('should set city and stateUSA state correctly', () => {
       const location = 'San Diego, CA';
@@ -72,8 +72,8 @@ describe('App tests', () => {
 
       renderedApp.instance().updateLocation(location);
       
-      expect(renderedApp.state('stateUSA')).toEqual(stateExpectation)
-      expect(renderedApp.state('city')).toEqual(cityExpectation)
-    }) 
-  })
-})
+      expect(renderedApp.state('stateUSA')).toEqual(stateExpectation);
+      expect(renderedApp.state('city')).toEqual(cityExpectation);
+    }); 
+  });
+});
--- test/cleanData.test.js
+++ test/cleanData.test.js
@@ -2,7 +2,6 @@ import { shallow, mount } from 'enzyme';
 import App from '../lib/App.js';
 import React from 'react';
 import cleanedData from '../lib/mock-data';
-import cleanData from '../lib/cleanData';
 require('locus');
 
 describe('Data cleaner', () => {
@@ -11,98 +10,96 @@ describe('Data cleaner', () => {
   beforeEach(() => {
     renderedApp = shallow(<App />);
     renderedApp.setState({data: cleanedData});
-  })
+  });
 
   describe('Current data', () => { 
 
     it('should set the current city to the inputted city name', () => {
       const expectation = 'San Diego';
 
-      expect(renderedApp.state('data').current.city).toEqual(expectation)
-    })
+      expect(renderedApp.state('data').current.city).toEqual(expectation);
+    });
 
     it('should set the current state to the inputted state name', () => {
       const expectation = 'California';
 
-      expect(renderedApp.state('data').current.state).toEqual(expectation)
-    })
+      expect(renderedApp.state('data').current.state).toEqual(expectation);
+    });
 
     it('should set the current temperature', () => {
       const expectation = 60.8;
 
-      expect(renderedApp.state('data').current.temp).toEqual(expectation)
-    })
+      expect(renderedApp.state('data').current.temp).toEqual(expectation);
+    });
 
     it('should set the current condition', () => {
       const expectation = 'Mostly Cloudy';
 
-      expect(renderedApp.state('data').current.currentCondition).toEqual(expectation)
-    })
+      expect(renderedApp.state('data').current.currentCondition).toEqual(expectation);
+    });
 
     it('should return current weather details', () => {
       const expectation = 'Mostly Cloudy';
 
       expect(renderedApp.state('data').current.currentWeatherDetails.length).toEqual(6);
-    })
+    });
 
     it('should return the high temp for the day', () => {
       const expectation = '62';
 
-      expect(renderedApp.state('data').forecast.highTemp).toEqual(expectation)
-    })
+      expect(renderedApp.state('data').forecast.highTemp).toEqual(expectation);
+    });
 
     it('should return the low temp for the day', () => {
       const expectation = '54';
 
-      expect(renderedApp.state('data').forecast.lowTemp).toEqual(expectation)
-    })
+      expect(renderedApp.state('data').forecast.lowTemp).toEqual(expectation);
+    });
 
     it('should return the current day', () => {
       const expectation = 'Monday';
 
       expect(renderedApp.state('data').forecast.day).toEqual(expectation);
-    })
+    });
 
     it('should return current day number', () => {
       const expectation = 16;
 
       expect(renderedApp.state('data').forecast.dayInteger).toEqual(expectation);
-    })
+    });
 
-  })
+  });
 
   describe('Ten day forecast data', () => { 
 
-    it('should return an array with objects containing the next 7 days of weather data', () => {
+    it('should return an array with objects containing the next 10 days of weather data', () => {
 
       expect(renderedApp.state('data').forecast.tenDayArray.length).toEqual(10);    
-    })
+    });
 
-  })
+  });
 
-  // describe('Forecast data', () => { 
+  describe('Hourly forecast data', () => { 
 
-  //   it('should set the current temperature', () => {
-  //     renderedApp.setState({data: cleanedData});
-  //     const expectation = 60.8;
+    it('should return an array with objects containing the next 7 hours of weather data', () => {
+      renderedApp.setState({data: cleanedData});
 
-  //     expect(renderedApp.state('data').current.temp).toEqual(expectation)
-  //   })
+      expect(renderedApp.state('data').hourly.length).toEqual(7);   
+    });
 
-  //   it('should set the current condition', () => {
-  //     renderedApp.setState({data: cleanedData});
-  //     const expectation = 'Mostly Cloudy';
+    it('should return data in the right format for each hour', () => {
+      renderedApp.setState({data: cleanedData});
+      const expected = {
+        "ampm": "AM", 
+        "condition": "Mostly Cloudy", 
+        "hour": "11", 
+        "temp": "64"
+      };
+      
+      expect(renderedApp.state('data').hourly[0]).toEqual(expected);   
+    });
 
-  //     expect(renderedApp.state('data').current.currentCondition).toEqual(expectation)
-  //   })
+  });
 
-  //   it('should return current weather details', () => {
-  //     renderedApp.setState({data: cleanedData});
-  //     const expectation = 'Mostly Cloudy';
-
-  //     expect(renderedApp.state('data').current.currentWeatherDetails.length).toEqual(6);
-  //   })
-  // })
-
-})
+});
 
