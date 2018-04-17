2037a5b646c2c9d0bba24ed31f4dc4e8f7d16977
Tyler McNamara <mcnamara14@gmail.com>
2018-04-16 21:26:57 -0600

    Add current data testing

--- test/cleanData.test.js
+++ test/cleanData.test.js
@@ -1,6 +1,7 @@
 import { shallow, mount } from 'enzyme';
 import App from '../lib/App.js';
 import React from 'react';
+import cleanedData from '../lib/mock-data';
 import cleanData from '../lib/cleanData';
 require('locus');
 
@@ -9,20 +10,99 @@ describe('Data cleaner', () => {
 
   beforeEach(() => {
     renderedApp = shallow(<App />);
+    renderedApp.setState({data: cleanedData});
   })
 
   describe('Current data', () => { 
 
-    it('should clean the API data file to the desired specs', () => {
-      const location = 'San Diego, CA';
+    it('should set the current city to the inputted city name', () => {
       const expectation = 'San Diego';
-
-      renderedApp = mount(<App />)
-
-      renderedApp.instance().updateLocation(location);
 
       expect(renderedApp.state('data').current.city).toEqual(expectation)
     })
+
+    it('should set the current state to the inputted state name', () => {
+      const expectation = 'California';
+
+      expect(renderedApp.state('data').current.state).toEqual(expectation)
+    })
+
+    it('should set the current temperature', () => {
+      const expectation = 60.8;
+
+      expect(renderedApp.state('data').current.temp).toEqual(expectation)
+    })
+
+    it('should set the current condition', () => {
+      const expectation = 'Mostly Cloudy';
+
+      expect(renderedApp.state('data').current.currentCondition).toEqual(expectation)
+    })
+
+    it('should return current weather details', () => {
+      const expectation = 'Mostly Cloudy';
+
+      expect(renderedApp.state('data').current.currentWeatherDetails.length).toEqual(6);
+    })
+
+    it('should return the high temp for the day', () => {
+      const expectation = '62';
+
+      expect(renderedApp.state('data').forecast.highTemp).toEqual(expectation)
+    })
+
+    it('should return the low temp for the day', () => {
+      const expectation = '54';
+
+      expect(renderedApp.state('data').forecast.lowTemp).toEqual(expectation)
+    })
+
+    it('should return the current day', () => {
+      const expectation = 'Monday';
+
+      expect(renderedApp.state('data').forecast.day).toEqual(expectation);
+    })
+
+    it('should return current day number', () => {
+      const expectation = 16;
+
+      expect(renderedApp.state('data').forecast.dayInteger).toEqual(expectation);
+    })
+
   })
+
+  describe('Ten day forecast data', () => { 
+
+    it('should return an array with objects containing the next 7 days of weather data', () => {
+
+      expect(renderedApp.state('data').forecast.tenDayArray.length).toEqual(10);    
+    })
+
+  })
+
+  // describe('Forecast data', () => { 
+
+  //   it('should set the current temperature', () => {
+  //     renderedApp.setState({data: cleanedData});
+  //     const expectation = 60.8;
+
+  //     expect(renderedApp.state('data').current.temp).toEqual(expectation)
+  //   })
+
+  //   it('should set the current condition', () => {
+  //     renderedApp.setState({data: cleanedData});
+  //     const expectation = 'Mostly Cloudy';
+
+  //     expect(renderedApp.state('data').current.currentCondition).toEqual(expectation)
+  //   })
+
+  //   it('should return current weather details', () => {
+  //     renderedApp.setState({data: cleanedData});
+  //     const expectation = 'Mostly Cloudy';
+
+  //     expect(renderedApp.state('data').current.currentWeatherDetails.length).toEqual(6);
+  //   })
+  // })
+
 })
 
