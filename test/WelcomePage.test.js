106409d74b04a54121a6f7fb8f2a804e67aae8ff
Helen Dechat <hdechat@gmail.com>
2018-04-16 22:40:14 -0600

    Add WelcomePage tests

--- test/WelcomePage.test.js
+++ test/WelcomePage.test.js
@@ -1,6 +1,5 @@
 import { shallow, mount } from 'enzyme';
 import WelcomePage from '../lib/WelcomePage.js';
-import Location from '../lib/Location.js'
 import React from 'react';
 
 require('locus');
@@ -21,6 +20,33 @@ describe('Welcome Page tests', () => {
     it('should render the Location component on new visit', () => {
 
       expect(renderedWelcome.find('Location').length).toEqual(1);
+    });
+
+    it('should include one main element', () => {
+
+      expect(renderedWelcome.find('main').length).toEqual(1);
+    });
+
+    it('should include one ErrorHandler components', () => {
+
+      expect(renderedWelcome.find('ErrorHandler').length).toEqual(1);
+    });
+
+    it('should include one div', () => {
+
+      expect(renderedWelcome.find('div').length).toEqual(1);
+    });
+
+    it('should include one h1 with text inside div', () => {
+      const expectation = 'Weathrly';
+
+      expect(renderedWelcome.find('div h1').text()).toEqual(expectation);
+    });
+
+    it('should include one p with text inside div', () => {
+      const expectation = 'Enter a location to view the forecast';
+
+      expect(renderedWelcome.find('div p').text()).toEqual(expectation);
     });
   });
 });
